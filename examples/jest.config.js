c01e5906f2f2d24e7ff21a5de671b6d1a433f9c0
Naffar, Nabil (nn605g) <nn605g@intl.att.com>
2018-10-16 11:58:06 +0300

    examples dir added + angular monorepo

---
+++ examples/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testURL: 'http://localhost',
+  preset: 'jest-preset-angular',
+  setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
+  roots: ['src/app', 'projects'],
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/*.module.ts',
+    '!**/app.routing.ts',
+    '!**/*.d.ts',
+    '!src/setupJest.ts',
+    '!src/app/models/*'
+  ]
+};
