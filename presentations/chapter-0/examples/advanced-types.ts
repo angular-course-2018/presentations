8a90731d4e44cf9cab0986783f090d3419086b2c
Danny Vernovsky <dvernovsky@interwise.com>
2018-10-18 11:54:35 +0300

    typescript examples

---
+++ presentations/chapter-0/examples/advanced-types.ts
@@ -0,0 +1,62 @@
+export interface Talkable {
+    talk(): void;
+}
+
+enum Gender {
+    Female,
+    Male
+}
+
+enum GenderStr {
+    Female = 'Female',
+    Male = 'Male'
+}
+
+export abstract class Person implements Talkable {
+    // public by default
+    firstname: string;
+
+    get fullname() {
+        return `${this.firstname} ${this.lastname}`;
+    }
+
+    // last name will be also public and will be autoassigned to this.lastname
+    constructor(firstname: string, public lastname: string) {
+        this.firstname = firstname;
+    }
+
+    // public function
+    talk() {
+        console.log(`i'm person`);
+    }
+
+    // public property with function as a value
+    walk = () => {
+
+    }
+}
+
+export class Teacher extends Person {
+
+    // readonly variables can only be set inside contructor and cannot be chaged later.
+    constructor(readonly school: string, firstname: string, lastname: string) {
+        // must call super as it defines custom contructor
+        super(firstname, lastname);
+    }
+
+    // this function will hide the function on parent
+    talk() {
+        console.log(`i'm teacher`);
+    }
+
+    teach() {
+
+    }
+}
+
+
+const teacher = new Teacher('Awesome School', 'Michael', 'Jackson');
+teacher.talk();
+console.log(teacher.fullname);
+
+// (teacher as Person).talk();
---
+++ presentations/chapter-0/examples/basics.js
@@ -0,0 +1,51 @@
+//var, const, let
+
+if (true) {
+    var imEverywhere = [];
+}
+
+for (var i = 0; i < 10; i++) {
+    imEverywhere.push(i);
+}
+
+console.log(`var: ${imEverywhere}`);
+console.log(`i: ${i}`);
+
+imEverywhere = [];
+
+console.log(`new array: ${JSON.stringify(imEverywhere)}`);
+
+// array spread
+const arr = [1, 2];
+
+const anotherArr = [...arr, 5, 6];
+
+function sum(a, b, c, d) {
+    return a + b + c + d;
+}
+
+console.log(`sum: ${sum(...anotherArr)}`);
+
+//destructuring
+
+let first;
+let second;
+let rest;
+
+[first, second, ...rest] = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(`first: ${first}`);
+console.log(`second: ${second}`);
+console.log(`rest: ${rest}`);
+
+//symbols
+
+const symbol1 = Symbol();
+const symbol2 = Symbol(42);
+const symbol3 = Symbol('foo');
+
+console.log(`type of: ${typeof symbol1}`);
+
+console.log(`tostring: ${symbol3.toString()}`);
+
+console.log(`equality: ${Symbol('foo') === Symbol('foo')}`);
