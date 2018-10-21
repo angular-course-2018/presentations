b9c9e3d299dc3dabc8d7e8ee5ad71caa25b39e82
Naffar, Nabil (nn605g) <nn605g@intl.att.com>
2018-10-21 14:25:42 +0300

    rxjs

--- examples/projects/chapter4/src/app/app.module.ts
+++ examples/projects/chapter4/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FibonacciGeneratorComponent } from './fibonacci-generator/fibonacci-generator.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FibonacciGeneratorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ examples/projects/chapter4/src/app/fibonacci-generator/fibonacci-generator.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+import { scan, tap, pluck, takeWhile } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-fibonacci-generator-component',
+    styleUrls: ['./fibonacci-generator.component.scss'],
+    templateUrl: './fibonacci-generator.component.html'
+})
+export class FibonacciGeneratorComponent {
+    series$: Observable<number[]>;
+    numOfItems = 5;
+
+    start() {
+        this.series$ = interval(500)
+            .pipe(
+                takeWhile(val => val < this.numOfItems),
+                scan(x => [x[1], x[0] + x[1]], [0, 1]),
+                pluck('0'),
+                scan((acc, curr) => [...acc, curr], []),
+                tap(a => this.logger(a)),
+            );
+    }
+
+    logger(message) {
+        console.log('FibonacciGeneratorComponent: ', message);
+    }
+}
+
